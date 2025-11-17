0326d5004d602f966d2cf481590e06b706831f4c
Salma001-it <salmanajih001@gmail.com>
2025-11-17 16:09:24 +0100

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import time
 import pandas as pd
 import csv
-
 options = Options()
-options.binary_location = "/usr/bin/chromium-browser"
 options.add_argument("--headless")
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=options)
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
 from datetime import date
 
 oggi = date.today()
